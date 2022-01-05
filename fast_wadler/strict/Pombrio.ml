feecb6b8d6562daae588a3a06301ca9664da5fa4
Justin <zallambo@gmail.com>
2022-01-05 17:49:00 -0500

    Improve strict impls.
    - Implement DocChoice
    - Make string concatenation non-quadratic

--- fast_wadler/strict/Pombrio.ml
+++ fast_wadler/strict/Pombrio.ml
@@ -9,7 +9,6 @@ type measure = {
 }
 
 let empty_m = { flat = 0; nonflat = None }
-
 let flatten_m { flat; nonflat } = { flat = flat; nonflat = None }
 
 let add_suff f s1 s2 = match (s1, s2) with
@@ -33,39 +32,41 @@ type doc =
   | DocNil
   | DocText of string
   | DocBreak of string
+  | DocFlatten of doc * measure
   | DocNest of int * doc * measure
-  | DocGroup of doc * measure
-  | DocCons of doc * doc * measure
-
-let strlen  = String.length
-let nl      = "\n"
+  | DocConcat of doc * doc * measure
+  | DocChoice of doc * doc * measure
 
 let measure d = match d with
-  | DocNil           -> empty_m
-  | DocText(s)       -> { flat = strlen s; nonflat = None }
-  | DocBreak(s)      -> { flat = strlen s; nonflat = Some(0) }
-  | DocNest(_, _, m) -> m
-  | DocGroup(_, m)   -> m
-  | DocCons(_, _, m) -> m
+  | DocNil             -> empty_m
+  | DocText(s)         -> { flat = String.length s; nonflat = None }
+  | DocBreak(s)        -> { flat = String.length s; nonflat = Some(0) }
+  | DocFlatten(_, m)   -> m
+  | DocNest(_, _, m)   -> m
+  | DocConcat(_, _, m) -> m
+  | DocChoice(_, _, m) -> m
 
 let empty       = DocNil
 let text s      = DocText(s)
+let nest i x    = DocNest(i, x, measure x)
 let break       = DocBreak(" ")
 let breakWith s = DocBreak(s)
-let nest i x    = DocNest(i, x, measure x)
-let (^^) x y    = DocCons(x, y, add_m (measure x) (measure y))
-let group d     = DocGroup(d, measure d)
+let flatten d   = DocFlatten(d, flatten_m (measure d))
+let (^^) x y    = DocConcat(x, y, add_m (measure x) (measure y))
+let (^?) x y    = DocChoice(x, y, measure y)
+let group d     = flatten d ^? d
 
 type sdoc =
   | SNil
   | SText of string * sdoc
   | SLine of int * sdoc
 
-let rec sdocToString = function
-  | SNil        -> ""
-  | SText(s, d) -> s ^ sdocToString d
-  | SLine(i, d) -> let prefix = String.make i ' '
-                   in nl ^ prefix ^ sdocToString d
+let rec sdocToList = function
+  | SNil -> []
+  | SText(s, d) -> s :: sdocToList d
+  | SLine(i, d) -> "\n" :: String.make i ' ' :: sdocToList d
+
+let sdocToString d = String.concat "" (sdocToList d)
 
 (* The width until the earliest possible newline, or end of document. *)
 let suffix_len = function
@@ -76,32 +77,39 @@ let suffix_len = function
  * Pretty Printing *
  *******************)
 
+type mode = | Flat | Ind of int
+
 let rec format w k = function
-  | []                                  -> SNil
-  | (i, m, DocNil)                 :: z -> format w k z
-  | (i, m, DocText(s))             :: z -> SText(s, format w (k + strlen s) z)
-  | (None, m, DocBreak(s))         :: z -> SText(s, format w (k + strlen s) z)
-  | (Some(i), m, DocBreak(s))      :: z -> SLine(i, format w i z)
-  | (None, m, DocNest(j, x, _))    :: z -> format w k ((None, m, x) :: z)
-  | (Some(i), m, DocNest(j, x, _)) :: z -> format w k ((Some(i+j), m, x) :: z)
-  | (i, m, DocCons(x, y, _))       :: z ->
+  | []                                 -> SNil
+  | (i, m, DocNil)                :: z -> format w k z
+  | (i, m, DocText(s))            :: z -> SText(s, format w (k + String.length s) z)
+  | (Flat, m, DocBreak(s))        :: z -> SText(s, format w (k + String.length s) z)
+  | (Ind(i), m, DocBreak(s))      :: z -> SLine(i, format w i z)
+  | (Flat, m, DocNest(j, x, _))   :: z -> format w k ((Flat, m, x) :: z)
+  | (Ind(i), m, DocNest(j, x, _)) :: z -> format w k ((Ind(i+j), m, x) :: z)
+  | (i, m, DocConcat(x, y, _))    :: z ->
     let mx = add_m (measure y) m in
     format w k ((i, mx, x) :: (i, m, y) :: z)
-  | (i, m, DocGroup(x, _))         :: z ->
-    if k + suffix_len (add_m (flatten_m (measure x)) m) <= w
-    then format w k ((None, m, x) :: z)
-    else format w k ((i, m, x) :: z)
+  | (i, m, DocFlatten(x, _))      :: z -> format w k ((Flat, m, x) :: z)
+  | (i, m, DocChoice(x, y, _))    :: z ->
+    if k + suffix_len (add_m (measure x) m) <= w
+    then format w k ((i, m, x) :: z)
+    else format w k ((i, m, y) :: z)
+
+let pretty w doc =
+  let sdoc = format w 0 [(Ind(0), empty_m, doc)] in
+  sdocToString sdoc
+
+(*************
+ * Test Case *
+ *************)
 
 let rec bigdoc n =
   if n == 0
   then text "<leaf/>"
   else group
     ((nest 2 (text "<branch>" ^^ break ^^ bigdoc (n-1) ^^ break ^^ bigdoc (n-1)))
-     ^^ break ^^ text "</branch>")
+      ^^ break ^^ text "</branch>");;
 
-let pretty w doc =
-  let sdoc = format w 0 [(Some(0), empty_m, doc)] in
-  sdocToString sdoc
-
-;;
-print_endline (pretty 80 (bigdoc 12))
+print_endline (pretty 80 (bigdoc 14));;
+(* On laptop: 0.13s real time, 0.04s user time *)
--- fast_wadler/strict/StrictWadler.ml
+++ fast_wadler/strict/StrictWadler.ml
@@ -1,34 +1,36 @@
 (* Strictly Pretty, Christian Lindig, 2000 *)
+(* Slightly modified for readability by Justin *)
 
 type doc =
   | DocNil
-  | DocCons of doc * doc
   | DocText of string
-  | DocNest of int * doc
   | DocBreak of string
-  | DocGroup of doc
+  | DocFlatten of doc
+  | DocNest of int * doc
+  | DocConcat of doc * doc
+  | DocChoice of doc * doc
 
-let strlen  = String.length
-let nl      = "\n"
-
-let (^^) x y    = DocCons(x, y)
 let empty       = DocNil
 let text s      = DocText(s)
 let nest i x    = DocNest(i, x)
 let break       = DocBreak(" ")
 let breakWith s = DocBreak(s)
-let group d     = DocGroup(d)
+let flatten d   = DocFlatten(d)
+let (^^) x y    = DocConcat(x, y)
+let (^?) x y    = DocChoice(x, y)
+let group d     = flatten d ^? d
 
 type sdoc =
   | SNil
   | SText of string * sdoc
   | SLine of int * sdoc
 
-let rec sdocToString = function
-  | SNil        -> ""
-  | SText(s, d) -> s ^ sdocToString d
-  | SLine(i, d) -> let prefix = String.make i ' '
-                   in nl ^ prefix ^ sdocToString d
+let rec sdocToList = function
+  | SNil -> []
+  | SText(s, d) -> s :: sdocToList d
+  | SLine(i, d) -> "\n" :: String.make i ' ' :: sdocToList d
+
+let sdocToString d = String.concat "" (sdocToList d)
 
 type mode = | Flat | Break
 
@@ -36,35 +38,41 @@ let rec fits w = function
   | _ when w < 0                 -> false
   | []                           -> true
   | (i, m, DocNil)          :: z -> fits w z
-  | (i, m, DocCons(x, y))   :: z -> fits w ((i, m, x) :: (i, m, y) :: z)
+  | (i, m, DocConcat(x, y)) :: z -> fits w ((i, m, x) :: (i, m, y) :: z)
   | (i, m, DocNest(j, x))   :: z -> fits w ((i+j, m, x) :: z)
-  | (i, m, DocText(s))      :: z -> fits (w - strlen s) z
-  | (i, Flat, DocBreak(s))  :: z -> fits (w - strlen s) z
+  | (i, m, DocText(s))      :: z -> fits (w - String.length s) z
+  | (i, Flat, DocBreak(s))  :: z -> fits (w - String.length s) z
   | (i, Break, DocBreak(_)) :: z -> true (*impossible*)
-  | (i, m, DocGroup(x))     :: z -> fits w ((i, Flat, x) :: z)
+  | (i, m, DocFlatten(x))   :: z -> fits w ((i, Flat, x) :: z)
+  | (i, m, DocChoice(x, y)) :: z -> fits w ((i, m, y) :: z)
 
 let rec format w k = function
   | []                           -> SNil
   | (i, m, DocNil)          :: z -> format w k z
-  | (i, m, DocCons(x, y))   :: z -> format w k ((i, m, x) :: (i, m, y) :: z)
+  | (i, m, DocConcat(x, y)) :: z -> format w k ((i, m, x) :: (i, m, y) :: z)
   | (i, m, DocNest(j, x))   :: z -> format w k ((i+j, m, x) :: z)
-  | (i, m, DocText(s))      :: z -> SText(s, format w (k + strlen s) z)
-  | (i, Flat, DocBreak(s))  :: z -> SText(s, format w (k + strlen s) z)
+  | (i, m, DocText(s))      :: z -> SText(s, format w (k + String.length s) z)
+  | (i, Flat, DocBreak(s))  :: z -> SText(s, format w (k + String.length s) z)
   | (i, Break, DocBreak(s)) :: z -> SLine(i, format w i z)
-  | (i, m, DocGroup(x))     :: z -> if fits (w-k) ((i, Flat, x) :: z)
-                                    then format w k ((i, Flat, x) :: z)
-                                    else format w k ((i, Break, x) :: z)
+  | (i, m, DocFlatten(x))   :: z -> format w k ((i, Flat, x) :: z)
+  | (i, m, DocChoice(x, y)) :: z -> if fits (w-k) ((i, m, x) :: z)
+                                    then format w k ((i, m, x) :: z)
+                                    else format w k ((i, m, y) :: z)
+
+let pretty w doc =
+  let sdoc = format w 0 [0, Break, doc] in
+  sdocToString sdoc
+
+(*************
+ * Test Case *
+ *************)
 
 let rec bigdoc n =
   if n == 0
   then text "<leaf/>"
   else group
     ((nest 2 (text "<branch>" ^^ break ^^ bigdoc (n-1) ^^ break ^^ bigdoc (n-1)))
-     ^^ break ^^ text "</branch>")
+      ^^ break ^^ text "</branch>");;
 
-let pretty w doc =
-  let sdoc = format w 0 [0, Flat, DocGroup(doc)] in
-  sdocToString sdoc
-
-;;
-print_endline (pretty 80 (bigdoc 12))
+print_endline (pretty 80 (bigdoc 14));;
+(* On laptop: 0.12s real time, 0.03s user time *)
